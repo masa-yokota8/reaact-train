86a37ba93d53e22f18d8e6903121c656e1bb93db
masa-yokota8 <54167425+masa-yokota8@users.noreply.github.com>
2022-09-22 01:58:56 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+const root = document.getElementById("root");
+
+ReactDom.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  root
+);
